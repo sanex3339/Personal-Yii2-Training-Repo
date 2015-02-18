5762232c10044f805e195e323ddbb29b04c57c94
sanex3339 <sanex3339@yandex.ru>
2015-02-19 00:55:56 +0300

    Added extensions
    Added extensions.php

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,110 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'sanex/yii-layout' => 
+  array (
+    'name' => 'sanex/yii-layout',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@sanex/layout' => $vendorDir . '/sanex/yii-layout',
+    ),
+    'bootstrap' => 'sanex\\layout\\Bootstrap',
+  ),
+  'sanex/yii-site-module' => 
+  array (
+    'name' => 'sanex/yii-site-module',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@sanex/site' => $vendorDir . '/sanex/yii-site-module',
+    ),
+    'bootstrap' => 'sanex\\site\\Bootstrap',
+  ),
+  'sanex/yii-hexal-module' => 
+  array (
+    'name' => 'sanex/yii-hexal-module',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@sanex/hexal' => $vendorDir . '/sanex/yii-hexal-module',
+    ),
+    'bootstrap' => 'sanex\\hexal\\Bootstrap',
+  ),
+  'sanex/yii-blog-module' => 
+  array (
+    'name' => 'sanex/yii-blog-module',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@sanex/blog' => $vendorDir . '/sanex/yii-blog-module',
+    ),
+    'bootstrap' => 'sanex\\blog\\Bootstrap',
+  ),
+  'sanex/yii-catalog-module' => 
+  array (
+    'name' => 'sanex/yii-catalog-module',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@sanex/catalog' => $vendorDir . '/sanex/yii-catalog-module',
+    ),
+    'bootstrap' => 'sanex\\catalog\\Bootstrap',
+  ),
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'yiisoft/yii2-codeception' => 
+  array (
+    'name' => 'yiisoft/yii2-codeception',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/codeception' => $vendorDir . '/yiisoft/yii2-codeception',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+);
